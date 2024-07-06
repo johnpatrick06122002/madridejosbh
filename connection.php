a5d86ef5cfe847de198e680a4199dfa2f35a490b
kynducz <kenethducay12@gmail.com>
2024-07-06 21:18:21 +0800

    commit

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', ' u510162695_bhouse_root');
+define('DB_USERNAME', 'u510162695_bhouse_root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'u510162695_bhouse');
 $dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
