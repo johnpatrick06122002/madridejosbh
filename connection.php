42d6c6a87d1d2eb95f490b18625b6cc172a3c926
kynducz <kenethducay12@gmail.com>
2024-07-08 11:47:50 +0800

    commit

--- connection.php
+++ connection.php
@@ -1,11 +1,14 @@
 <?php
 
-define('DB_SERVER', ' 127.0.0.1');
+define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'u510162695_bhouse_root');
 define('DB_PASSWORD', '1Bhouse_root');
 define('DB_NAME', 'u510162695_bhouse');
+
 $dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-if($dbconnection === false){
+
+// Check connection
+if ($dbconnection === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
