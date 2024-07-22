9ccd3d334c2427cd9e6682a7ce924237bc707243
kynducz <kenethducay12@gmail.com>
2024-07-22 18:36:09 +0800

    d

--- admin/header.php
+++ admin/header.php
@@ -5,6 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="shortcut icon" type="image/x-icon" href="../bhh.jpg">
 	<title>ADMIN</title>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
 <link rel="stylesheet" href="../src/richtext.min.css">
--- landlord/header.php
+++ landlord/header.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="../bhh.jpg">
     <title>LANDLORD</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
