15663f46d53f8ab156171b9e18b20eeb9df9bc14
kynducz <kenethducay12@gmail.com>
2024-10-09 16:55:55 +0800

    commit

--- connection.php
+++ connection.php
@@ -1,19 +1,19 @@
 <?php
 // Check if constants are already defined before defining them
 if (!defined('DB_SERVER')) {
-    define('DB_SERVER', 'localhost');
+    define('DB_SERVER', '127.0.0.1');
 }
 
 if (!defined('DB_USERNAME')) {
-    define('DB_USERNAME', 'root');
+    define('DB_USERNAME', 'u510162695_bhouse_root');
 }
 
 if (!defined('DB_PASSWORD')) {
-    define('DB_PASSWORD', '');
+    define('DB_PASSWORD', '1Bhouse_root');
 }
 
 if (!defined('DB_NAME')) {
-    define('DB_NAME', 'h2');
+    define('DB_NAME', 'u510162695_bhouse');
 }
 
 $dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
