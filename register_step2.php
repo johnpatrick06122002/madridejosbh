3425666c921d16b4fe49ca58305b21015a4bf40b
kynducz <kenethducay12@gmail.com>
2024-10-12 18:28:19 +0800

    commit

--- register_step2.php
+++ register_step2.php
@@ -21,7 +21,7 @@ if (isset($_POST['submit'])) {
 
     // Handle file uploads
     if ($profile_photo) {
-        move_uploadsed_file($_FILES['profile_photo']['tmp_name'], "uploads/" . basename($profile_photo));
+        move_uploaded_file($_FILES['profile_photo']['tmp_name'], "uploads/" . basename($profile_photo));
     }
 
     // Get the ID from the `register1` table using the session email
