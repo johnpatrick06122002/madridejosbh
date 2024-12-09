017fc078b431fdf9cfb1d65261c78e8a68e2f2dd
kynducz <kenethducay12@gmail.com>
2024-12-09 11:45:20 +0800

    commit

---
+++ sql.php
@@ -0,0 +1,51 @@
+<?php
+// Check if constants are already defined before defining them
+if (!defined('DB_SERVER')) {
+    define('DB_SERVER', '127.0.0.1');
+}
+
+if (!defined('DB_USERNAME')) {
+    define('DB_USERNAME', 'u510162695_bhouse_root');
+}
+
+if (!defined('DB_PASSWORD')) {
+    define('DB_PASSWORD', '1Bhouse_root');
+}
+
+if (!defined('DB_NAME')) {
+    define('DB_NAME', 'u510162695_bhouse');
+}
+
+// Establish database connection
+$dbconnection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+if ($dbconnection === false) {
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+// Query to get all columns of register1
+$query = "DESCRIBE register1";
+
+$result = mysqli_query($dbconnection, $query);
+
+if ($result) {
+    echo "<table border='1'>";
+    echo "<tr><th>Field</th><th>Type</th><th>Null</th><th>Key</th><th>Default</th><th>Extra</th></tr>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<tr>";
+        echo "<td>" . htmlspecialchars($row['Field']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['Type']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['Null']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['Key']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['Default']) . "</td>";
+        echo "<td>" . htmlspecialchars($row['Extra']) . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "ERROR: Could not fetch table fields. " . mysqli_error($dbconnection);
+}
+
+// Close the database connection
+mysqli_close($dbconnection);
+?>
