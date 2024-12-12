3cc5a767acc4f8d822e7e1bb5d3b86a2d843cb37
kynducz <kenethducay12@gmail.com>
2024-12-12 09:24:05 +0800

    commit

--- sql.php
+++ sql.php
@@ -68,7 +68,7 @@ echo "
 </style>";
 
 // Query to describe the table structure
-$describe_sql = "DESCRIBE register2";
+$describe_sql = "DESCRIBE subscriptions";
 $describe_result = mysqli_query($dbconnection, $describe_sql);
 
 if ($describe_result) {
@@ -92,7 +92,7 @@ if ($describe_result) {
 }
 
 // Query to fetch all data from the table
-$data_sql = "SELECT * FROM register2";
+$data_sql = "SELECT * FROM subscriptions";
 $data_result = mysqli_query($dbconnection, $data_sql);
 
 if ($data_result) {
