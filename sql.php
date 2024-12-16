cc3d849e00868bd0e7e618c5753fc1c79e914cc5
kynducz <kenethducay12@gmail.com>
2024-12-16 12:58:12 +0800

    commit

--- sql.php
+++ sql.php
@@ -26,30 +26,17 @@ if ($dbconnection === false) {
 
 // SQL query to create the 'booking' table
 $sql = "
-CREATE TABLE IF NOT EXISTS `booking` (
-  `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `book_ref_no` varchar(100) NOT NULL,
-  `payment_id` int(11) NOT NULL,
-  `firstname` varchar(100) NOT NULL,
-  `middlename` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) NOT NULL,
+CREATE TABLE IF NOT EXISTS  `otp` (
+  `otp_id` int(11) NOT NULL,
   `email` varchar(100) NOT NULL,
-  `age` int(11) NOT NULL,
-  `gender` varchar(225) NOT NULL,
-  `contact_number` varchar(80) NOT NULL,
-  `Address` varchar(255) NOT NULL,
-  `status` varchar(100) NOT NULL DEFAULT 'Pending',
-  `date_posted` datetime DEFAULT current_timestamp(),
-  `confirm_date` datetime DEFAULT NULL,
-  `otp_id` int(11) DEFAULT NULL,
-  `ratings` float NOT NULL,
-  `feedback` varchar(1000) NOT NULL
+  `otp` varchar(6) NOT NULL,
+  `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 ";
 
 // Execute the query
 if (mysqli_query($dbconnection, $sql)) {
-    echo "Table 'booking' created successfully.";
+    echo "Table 'otp' created successfully.";
 } else {
     echo "ERROR: Could not create table. " . mysqli_error($dbconnection);
 }
