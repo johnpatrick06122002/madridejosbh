aca800b4bb76e3517f288596b5dd60c59da7fab5
kynducz <kenethducay12@gmail.com>
2024-12-19 21:23:23 +0800

    commit

--- sql.php
+++ sql.php
@@ -21,36 +21,11 @@ if ($dbconnection === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
-// SQL to describe the `booking` table
-$sql = "DESCRIBE booking";
+// SQL to drop unique constraint on the `lastname` column
+$sql = "ALTER TABLE booking DROP INDEX lastname";
 
-if ($result = mysqli_query($dbconnection, $sql)) {
-    echo "<table border='1' cellpadding='5' cellspacing='0'>";
-    echo "<tr>
-            <th>Field</th>
-            <th>Type</th>
-            <th>Null</th>
-            <th>Key</th>
-            <th>Default</th>
-            <th>Extra</th>
-          </tr>";
-
-    // Fetch and display each row of the table structure
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "<tr>";
-        echo "<td>" . htmlspecialchars($row['Field']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['Type']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['Null']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['Key']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['Default']) . "</td>";
-        echo "<td>" . htmlspecialchars($row['Extra']) . "</td>";
-        echo "</tr>";
-    }
-
-    echo "</table>";
-
-    // Free result set
-    mysqli_free_result($result);
+if (mysqli_query($dbconnection, $sql)) {
+    echo "Unique constraint removed from 'lastname' column successfully.";
 } else {
     echo "ERROR: Could not execute $sql. " . mysqli_error($dbconnection);
 }
